132a7bf94e63e2052b346af437acc8cba3faa8e4
bozze007 <126372581+bozze007@users.noreply.github.com>
2024-02-18 00:10:24 +0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TheFile
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBoxPath = New System.Windows.Forms.TextBox()
+        Me.Buka = New System.Windows.Forms.Button()
+        Me.ListBoxItems = New System.Windows.Forms.ListBox()
+        Me.TextBoxEditor = New System.Windows.Forms.RichTextBox()
+        Me.Simpan = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBoxPath
+        '
+        Me.TextBoxPath.BackColor = System.Drawing.SystemColors.MenuText
+        Me.TextBoxPath.ForeColor = System.Drawing.SystemColors.InactiveBorder
+        Me.TextBoxPath.Location = New System.Drawing.Point(69, 46)
+        Me.TextBoxPath.Name = "TextBoxPath"
+        Me.TextBoxPath.Size = New System.Drawing.Size(455, 22)
+        Me.TextBoxPath.TabIndex = 0
+        '
+        'Buka
+        '
+        Me.Buka.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Buka.FlatAppearance.BorderSize = 0
+        Me.Buka.Location = New System.Drawing.Point(557, 46)
+        Me.Buka.Name = "Buka"
+        Me.Buka.Size = New System.Drawing.Size(135, 25)
+        Me.Buka.TabIndex = 1
+        Me.Buka.Text = "Buka"
+        Me.Buka.UseVisualStyleBackColor = False
+        '
+        'ListBoxItems
+        '
+        Me.ListBoxItems.BackColor = System.Drawing.SystemColors.InfoText
+        Me.ListBoxItems.ForeColor = System.Drawing.SystemColors.Window
+        Me.ListBoxItems.FormattingEnabled = True
+        Me.ListBoxItems.ItemHeight = 16
+        Me.ListBoxItems.Location = New System.Drawing.Point(69, 93)
+        Me.ListBoxItems.Name = "ListBoxItems"
+        Me.ListBoxItems.Size = New System.Drawing.Size(312, 324)
+        Me.ListBoxItems.TabIndex = 2
+        '
+        'TextBoxEditor
+        '
+        Me.TextBoxEditor.BackColor = System.Drawing.SystemColors.InfoText
+        Me.TextBoxEditor.ForeColor = System.Drawing.Color.Lime
+        Me.TextBoxEditor.Location = New System.Drawing.Point(410, 93)
+        Me.TextBoxEditor.Name = "TextBoxEditor"
+        Me.TextBoxEditor.Size = New System.Drawing.Size(361, 291)
+        Me.TextBoxEditor.TabIndex = 3
+        Me.TextBoxEditor.Text = ""
+        '
+        'Simpan
+        '
+        Me.Simpan.BackColor = System.Drawing.Color.YellowGreen
+        Me.Simpan.Location = New System.Drawing.Point(523, 390)
+        Me.Simpan.Name = "Simpan"
+        Me.Simpan.Size = New System.Drawing.Size(169, 27)
+        Me.Simpan.TabIndex = 4
+        Me.Simpan.Text = "Simpan"
+        Me.Simpan.UseVisualStyleBackColor = False
+        '
+        'TheFile
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Simpan)
+        Me.Controls.Add(Me.TextBoxEditor)
+        Me.Controls.Add(Me.ListBoxItems)
+        Me.Controls.Add(Me.Buka)
+        Me.Controls.Add(Me.TextBoxPath)
+        Me.Name = "TheFile"
+        Me.Text = "TheFile"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBoxPath As TextBox
+    Friend WithEvents Buka As Button
+    Friend WithEvents ListBoxItems As ListBox
+    Friend WithEvents TextBoxEditor As RichTextBox
+    Friend WithEvents Simpan As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,70 @@
+﻿'Nama: Muhammad Rafi Casey Susanto
+'NIM: 230401020014
+'Kelas: IT101 Pemrogramman Visual
+
+
+Imports System.IO
+
+Public Class TheFile
+    Private Sub Buka_Click(sender As Object, e As EventArgs) Handles Buka.Click
+        Dim path As String = TextBoxPath.Text
+
+        If Directory.Exists(path) Then
+            BukaFolder(path)
+        ElseIf File.Exists(path) Then
+            BukaFile(path)
+        Else
+            MessageBox.Show("Path tidak valid.")
+        End If
+    End Sub
+
+    Private Sub BukaFolder(path As String)
+        ListBoxItems.Items.Clear()
+        Dim folders() As String = Directory.GetDirectories(path)
+        Dim files() As String = Directory.GetFiles(path)
+
+        For Each folder In folders
+            ListBoxItems.Items.Add(IO.Path.GetFileName(folder) & " (Folder)")
+        Next
+
+        For Each file In files
+            ListBoxItems.Items.Add(IO.Path.GetFileName(file) & " (File)")
+        Next
+    End Sub
+
+    Private Sub BukaFile(path As String)
+        If IO.Path.GetExtension(path).ToLower() = ".txt" Then
+            Try
+                TextBoxEditor.Text = File.ReadAllText(path)
+                TextBoxPath.Text = path
+            Catch ex As Exception
+                MessageBox.Show("Tidak dapat membuka file.")
+            End Try
+        Else
+            MessageBox.Show("Hanya file teks (.txt) yang dapat dibuka dan diedit.")
+        End If
+    End Sub
+
+    Private Sub Simpan_Click(sender As Object, e As EventArgs) Handles Simpan.Click
+        Dim path As String = TextBoxPath.Text
+
+        If File.Exists(path) Then
+            Try
+                File.WriteAllText(path, TextBoxEditor.Text)
+                MessageBox.Show("Perubahan telah disimpan.")
+            Catch ex As Exception
+                MessageBox.Show("Tidak dapat menyimpan perubahan pada file.")
+            End Try
+        Else
+            MessageBox.Show("File tidak ditemukan.")
+        End If
+    End Sub
+
+    Private Sub TextBoxEditor_TextChanged(sender As Object, e As EventArgs) Handles TextBoxEditor.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
